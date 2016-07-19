923d8c6d422299bf32ba4a4996b2244f69f1fbee
Yat Chang <yatmchang@gmail.com>
2016-07-18 17:58:22 -0700

    added tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,12 +41,13 @@ class PostsController < ApplicationController
   end
 
   private
-  def post_params
-  params.require(:post).permit(:title, :body, :category_id)
-end
 
-def find_post
-  @post = Post.find params[:id]
-end
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id, {tag_ids: []})
+  end
+
+  def find_post
+    @post = Post.find params[:id]
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 ["Sports", "Art", "Pet", "Music", "Games", "Gardening", "Others", "Phones", "Cameras", "Magic"].each do |cat|
   Category.create name: cat
 end
+
+10.times { Tag.create(title: Faker::Hacker.adjective)}
